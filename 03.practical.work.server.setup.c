627d48d01d794c30525134aafecd579e9f0c893b
songuyen24 <nguyensonnty2@gmail.com>
2021-05-04 09:51:08 +0700

    lw3

--- 03.practical.work.server.setup.c
+++ 03.practical.work.server.setup.c
@@ -1,33 +1,49 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<stdlib.h>
+# define PORT 8784;
 
-int main (int argc, char **argv) {
+int main (int argc, char **argv)
+{
     int sockfd, clen, clientfd;
     struct sockaddr_in saddr, caddr;
     short port = 8784;
 
-    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    {
         printf ("Error creating socket\n");
         return -1;
     }
-
+     else
+    {
+        printf("Socket created successful\n");
+    }
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
     saddr.sin_port = htons(port);
 
-    if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)) {
+    if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0)) 
+    {
         printf("Error binding\n");
         return -1;
     }
 
-    if (listen(sockfd, 5) < 0) {
+        printf("Loading on port...%d \n", port);
+    if (listen(sockfd, 10) < 0) 
+    {
+
+    if (listen(sockfd, 5) < 0) 
+    {
         printf("Error listening\n");
         return -1;
+    }
+     else
+    {
+        printf("Listened succcessful\n");
     }
 
     clen=sizeof(caddr);
@@ -36,8 +52,11 @@ int main (int argc, char **argv) {
         printf("Error accepting connection\n");
         return -1;
     }
-
-    printf("The client connected\n");
-
-	return 0;
+     else
+     {
+        printf("Connected successful\n");
+        return 0;
+     }
+    }
 }
+
